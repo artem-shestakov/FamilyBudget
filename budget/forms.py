94b4b74b38d5e4f48da25eca016516cd25958172
Artem Shestakov <artem.s.shestakov@rt.ru>
2022-03-05 17:23:33 +0300

    Add income form validation of title

--- budget/forms.py
+++ budget/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from .models import Incomes
@@ -7,3 +8,8 @@ class IncomeForm(ModelForm):
     class Meta:
         model = Incomes
         fields = ['title']
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if Incomes.objects.filter(title=title).exists():
+            raise ValidationError(f"Income with title {title} already exists")
