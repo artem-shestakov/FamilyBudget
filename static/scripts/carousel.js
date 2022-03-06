d1a0a0302fc7df3fcae1e4c8940a660750be41dd
Artem Shestakov <artem.s.shestakov@rt.ru>
2022-03-06 18:17:38 +0300

    fix Uncaught SyntaxError: redeclaration of let

--- static/scripts/carousel.js
+++ static/scripts/carousel.js
@@ -1,10 +1,8 @@
-const prev  = document.querySelector('.prev');
-const next = document.querySelector('.next');
-
-const track = document.querySelector('.track');
-
-let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-let index = 0;
+prev  = document.querySelector('.prev');
+next = document.querySelector('.next');
+track = document.querySelector('.track');
+carouselWidth = document.querySelector('.carousel-container').offsetWidth;
+index = 0;
 
 window.onload = function() {
     if (track.offsetWidth - (index * carouselWidth) <= carouselWidth) {
