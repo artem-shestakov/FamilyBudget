b99013a37a41863ee3607403d7fc13ac318a6597
Artem Shestakov <artem.s.shestakov@rt.ru>
2022-03-09 17:26:13 +0300

    fix error with show closed toasts

--- static/scripts/toast.js
+++ static/scripts/toast.js
@@ -1,10 +1,34 @@
+function deleteHidenToast() {
+    var toastElList = [].slice.call(document.querySelectorAll('#toast'))
+    for (let toast in toastElList) {
+        if (toastElList[toast].classList.contains('hide')) {
+            toastElList[toast].outerHTML = "";
+        }
+    }
+}
+
+function showToasts() {
+    var toastElList = [].slice.call(document.querySelectorAll('#toast'))
+    var toastList = toastElList.map(function(toastEl) {
+        return new bootstrap.Toast(toastEl)
+    });
+    toastList.forEach(toast => toast.show());
+}
+
 ;(function(){
-    const toastEl = document.getElementById('toast')
+    const toastList = document.getElementById('toast-container')
+    const toastEl = document.getElementById('toast-tmp')
     const toastBody = document.getElementById('toastBody')
     const toast = new bootstrap.Toast(toastEl)
 
     htmx.on('showMessage', (e) => {
+        deleteHidenToast()
         toastBody.innerHTML = e.detail.value
-        toast.show()
+        let clone = toastEl.cloneNode( true );
+        clone.setAttribute( 'id', 'toast' );
+        toastList.appendChild(clone)
+
+        // toast.show()
+        showToasts()
     })
 })()
